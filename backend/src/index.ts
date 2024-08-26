36025f653c838dcb65505a03b7579961f6e32b5d
Pushkar <bansalpushkar100@gmail.com>
2024-08-26 18:10:46 +0530

    initial commit

---
+++ backend/src/index.ts
@@ -0,0 +1,10 @@
+import express, { Request, Response } from "express"
+
+const app = express()
+app.get('/', (req: Request, res: Response) => {
+    return res.send('API healthy')
+})
+
+app.listen(3000, () => {
+    console.log("Running on port 3000");
+})
