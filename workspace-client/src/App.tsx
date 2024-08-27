e2d5e62ba8d654b5242df53e3a2fb8dde28dcf0f
Pushkar <bansalpushkar100@gmail.com>
2024-08-27 20:49:28 +0530

    testing ci/cd

--- workspace-client/src/App.tsx
+++ workspace-client/src/App.tsx
@@ -16,7 +16,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React + Pushkar</h1>
+      <h1>Vite + React + Pushkar Bansal</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
