ba19d412625fdb1c177b00aa4efa78021a9898c2
Pushkar <bansalpushkar100@gmail.com>
2024-08-30 13:22:00 +0530

    improve readability

--- api/src/index.ts
+++ api/src/index.ts
@@ -18,35 +18,40 @@ wss.on("connection", (socket) => {
         const parseMsg = JSON.parse(message)
 
         if (parseMsg.type === 'create-room') {
-            const roomId = createRoomId()
+            // Creating Player1 that created the room 
             const deck = new Deck()
             const cards: any = deck.getPlayerCardset()
             const players = new Map();
             const player = { name: parseMsg.name, cards }
             players.set(socket, player)
+            
+            // Creating room and saving its details in-memory
+            const roomId = createRoomId()
             rooms.set(roomId, { players, deck })
+
             socket.send(JSON.stringify({ message: 'New room created', roomId, cards, players: [{ name: player.name, cardRemaining: player.cards.length }] }))
             console.log(rooms);
         }
 
         else if (parseMsg.type === 'join-room') {
             const roomId = parseMsg.roomId
-            if (!rooms.has(roomId))
-                socket.send('Invalid room Id, please try again')
-
+            // validate roomId
             const room: any = rooms.get(roomId);
             if (!room)
                 socket.send(JSON.stringify({ error: 'Invalid room id' }))
 
+            // Creating and adding player 2 that just joined
             const cards = room.deck.getPlayerCardset()
             room.players.set(socket, { name: parseMsg.name, cards })
-            const players: any[] = []
-            room.players.forEach((player: any, socket: Websocket) => {
-                players.push({ name: player.name, cardRemaining: player.cards.length })
-            })
-            socket.send(JSON.stringify({ message: 'Connected to room', roomId, cards, players }))
             rooms.set(roomId, room)
             console.log(rooms);
+
+            // creating response for client
+            const clientResponsePlayers: any[] = []
+            room.players.forEach((player: any, socket: Websocket) => {
+                clientResponsePlayers.push({ name: player.name, cardRemaining: player.cards.length })
+            })
+            socket.send(JSON.stringify({ message: 'Connected to room', roomId, cards, players:clientResponsePlayers }))
         }
     })
     socket.send(JSON.stringify({ message: 'Connected to ws server' }))
