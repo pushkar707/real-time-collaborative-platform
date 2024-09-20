87becfb5f62dbfd3e52a40d7350256ea404bb553
Pushkar <bansalpushkar100@gmail.com>
2024-09-21 01:39:13 +0530

    add: not-found page

---
+++ client/src/app/not-found.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function NotFound() {
+    return (
+        <div className='text-center py-12'>
+            <p className='text-2xl font-bold mb-3'>404 - Page Not Found</p>
+            <p>The page you're looking for doesn't exist.</p>
+        </div>
+    )
+}
