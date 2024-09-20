ef352a8556b94e1368323550247acccf4571b3cd
Pushkar <bansalpushkar100@gmail.com>
2024-09-21 01:42:22 +0530

    bug-fix

--- client/src/app/not-found.tsx
+++ client/src/app/not-found.tsx
@@ -3,8 +3,8 @@ import React from 'react'
 export default function NotFound() {
     return (
         <div className='text-center py-12'>
-            <p className='text-2xl font-bold mb-3'>404 &#39; Page Not Found</p>
-            <p>The page you're looking for doesn't exist.</p>
+            <p className='text-2xl font-bold mb-3'>404 - Page Not Found</p>
+            <p>The page you&#39;re looking for doesn&#39;t exist.</p>
         </div>
     )
 }
