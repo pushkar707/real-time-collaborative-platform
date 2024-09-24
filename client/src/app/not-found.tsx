31748324157db94a4e4bd311bb0b500ee680b86e
Pushkar <bansalpushkar100@gmail.com>
2024-09-25 02:15:50 +0530

    small fix

--- client/src/app/not-found.tsx
+++ client/src/app/not-found.tsx
@@ -5,6 +5,7 @@ export default function NotFound() {
         <div className='text-center py-12'>
             <p className='text-2xl font-bold mb-3'>404 - Page Not Found</p>
             <p>The page you&#39;re looking for doesn&#39;t exist.</p>
+            <p>Click <a href="/" className='underline text-blue-500'>here</a> to start a game</p>
         </div>
     )
 }
